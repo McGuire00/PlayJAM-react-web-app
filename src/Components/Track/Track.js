c0fdac3815facb2d7511554502ddd718ff1ebc53
Deron <deronmcguire23@yahoo.com>
2023-03-24 15:10:06 -0500

    Refactored Track class component to function component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,54 +1,95 @@
 import React from "react";
 import "./Track.css";
 
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-  }
-
-  renderAction() {
-    if (this.props.isRemoval) {
+export default function Track({ track, key, onAdd, onRemove, isRemoval }) {
+  function renderAction() {
+    if (isRemoval) {
       return (
-        <button className="Track-action" onClick={this.removeTrack}>
+        <button className="Track-action" onClick={removeTrack}>
           -
         </button>
       );
     } else {
       return (
-        <button className="Track-action" onClick={this.addTrack}>
+        <button className="Track-action" onClick={addTrack}>
           +
         </button>
       );
     }
   }
-  addTrack() {
-    this.props.onAdd(this.props.track);
+
+  function addTrack() {
+    onAdd(track);
   }
 
-  removeTrack() {
-    this.props.onRemove(this.props.track);
+  function removeTrack() {
+    onRemove(track);
   }
 
-  render() {
-    return (
-      <div className="Track">
-        <div className="Track-information">
-          <h3>{this.props.track.name}</h3>
-          {/* <p>
-            <img src={this.props.track.image} alt="#" />
-          </p> */}
-          <p>
-            <img src={this.props.track.image} alt="#" />
-            {this.props.track.artist} | {this.props.track.album}
-          </p>
-        </div>
-        {this.renderAction()}
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{track.name}</h3>
+        {/* <p>
+          <img src={this.props.track.image} alt="#" />
+        </p> */}
+        <p>
+          <img src={track.image} alt="#" />
+          {track.artist} | {track.album}
+        </p>
       </div>
-    );
-  }
+      {renderAction()}
+    </div>
+  );
 }
+// class Track extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-export default Track;
+//     this.addTrack = this.addTrack.bind(this);
+//     this.removeTrack = this.removeTrack.bind(this);
+//   }
+
+//   renderAction() {
+//     if (this.props.isRemoval) {
+//       return (
+//         <button className="Track-action" onClick={this.removeTrack}>
+//           -
+//         </button>
+//       );
+//     } else {
+//       return (
+//         <button className="Track-action" onClick={this.addTrack}>
+//           +
+//         </button>
+//       );
+//     }
+//   }
+//   addTrack() {
+//     this.props.onAdd(this.props.track);
+//   }
+
+//   removeTrack() {
+//     this.props.onRemove(this.props.track);
+//   }
+
+//   render() {
+//     return (
+//       <div className="Track">
+//         <div className="Track-information">
+//           <h3>{this.props.track.name}</h3>
+//           {/* <p>
+//             <img src={this.props.track.image} alt="#" />
+//           </p> */}
+//           <p>
+//             <img src={this.props.track.image} alt="#" />
+//             {this.props.track.artist} | {this.props.track.album}
+//           </p>
+//         </div>
+//         {this.renderAction()}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Track;
