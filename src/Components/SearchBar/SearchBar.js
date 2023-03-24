e19ec17dcbd11f3307e007f8f74814fbbd96a605
Deron <deronmcguire23@yahoo.com>
2023-03-24 14:38:48 -0500

    Added to gitignore and refactored SearchBar class component to functional componenet

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,39 +1,63 @@
 import React from "react";
+import { useState } from "react";
 import "./SearchBar.css";
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
+export default function SearchBar({ onSearch }) {
+  const [term, setTerm] = useState("");
 
-    this.state = {
-      term: "",
-    };
-
-    this.search = this.search.bind(this);
-    this.handleTermChange = this.handleTermChange.bind(this);
+  function search() {
+    onSearch(term);
   }
 
-  search() {
-    this.props.onSearch(this.state.term);
+  function handleTermChange(e) {
+    setTerm(e.target.value);
   }
 
-  handleTermChange(event) {
-    this.setState({ term: event.target.value });
-  }
-
-  render() {
-    return (
-      <div className="SearchBar">
-        <input
-          onChange={this.handleTermChange}
-          placeholder="Enter A Song, Album, or Artist"
-        />
-        <button className="SearchButton" onClick={this.search}>
-          SEARCH
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="SearchBar">
+      <input
+        onChange={handleTermChange}
+        placeholder="Enter A Song, Album, or Artist"
+      />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
+    </div>
+  );
 }
+// class SearchBar extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-export default SearchBar;
+//     this.state = {
+//       term: "",
+//     };
+
+//     this.search = this.search.bind(this);
+//     this.handleTermChange = this.handleTermChange.bind(this);
+//   }
+
+//   search() {
+//     this.props.onSearch(this.state.term);
+//   }
+
+//   handleTermChange(event) {
+//     this.setState({ term: event.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <div className="SearchBar">
+//         <input
+//           onChange={this.handleTermChange}
+//           placeholder="Enter A Song, Album, or Artist"
+//         />
+//         <button className="SearchButton" onClick={this.search}>
+//           SEARCH
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchBar
