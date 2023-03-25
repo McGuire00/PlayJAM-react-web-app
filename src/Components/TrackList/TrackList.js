b86d2edcc535a04a195773cea6beebdc633ab379
Deron <deronmcguire23@yahoo.com>
2023-03-24 20:15:58 -0500

    Refactored TrackList class component to functional component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,24 +3,41 @@ import "./TrackList.css";
 
 import Track from "../Track/Track";
 
-class TrackList extends React.Component {
-  render() {
-    return (
-      <div className="TrackList">
-        {this.props.tracks.map((track) => {
-          return (
-            <Track
-              track={track}
-              key={track.id}
-              onAdd={this.props.onAdd}
-              onRemove={this.props.onRemove}
-              isRemoval={this.props.isRemoval}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+export default function TrackList(props) {
+  return (
+    <div className="TrackList">
+      {props.tracks.map((track) => {
+        return (
+          <Track
+            track={track}
+            key={track.id}
+            onAdd={props.onAdd}
+            onRemove={props.onRemove}
+            isRemoval={props.isRemoval}
+          />
+        );
+      })}
+    </div>
+  );
 }
+// class TrackList extends React.Component {
+//   render() {
+//     return (
+//       <div className="TrackList">
+//         {this.props.tracks.map((track) => {
+//           return (
+//             <Track
+//               track={track}
+//               key={track.id}
+//               onAdd={this.props.onAdd}
+//               onRemove={this.props.onRemove}
+//               isRemoval={this.props.isRemoval}
+//             />
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
-export default TrackList;
+// export default TrackList;
