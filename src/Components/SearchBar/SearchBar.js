0bce4be210641538262e25ba2ca75964dd94db24
Deron <deronmcguire23@yahoo.com>
2023-03-30 12:26:26 -0500

    Added radio selection for choosing between creating new playlist or seeing users playlist

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "./SearchBar.css";
 
-export default function SearchBar({ onSearch }) {
+export default function SearchBar({ onSearch, handlePlaylistChange }) {
   const [term, setTerm] = useState("");
 
   function search() {
@@ -13,15 +13,43 @@ export default function SearchBar({ onSearch }) {
     setTerm(e.target.value);
   }
 
+  function handleOptionChange(e) {
+    handlePlaylistChange(e);
+  }
+
   return (
-    <div className="SearchBar">
-      <input
-        onChange={handleTermChange}
-        placeholder="Enter A Song, Album, or Artist"
-      />
-      <button className="SearchButton" onClick={search}>
-        SEARCH
-      </button>
+    <div className="container">
+      <div className="radio">
+        <label>
+          New Playlist
+          <input
+            type="radio"
+            name="playlist"
+            value="New Playlist"
+            defaultChecked={true}
+            onChange={handleOptionChange}
+          />
+        </label>
+        <label>
+          User's Playlists
+          <input
+            type="radio"
+            name="playlist"
+            value="User Playlist"
+            onChange={handleOptionChange}
+          />
+        </label>
+      </div>
+
+      <div className="SearchBar">
+        <input
+          onChange={handleTermChange}
+          placeholder="Enter A Song, Album, or Artist"
+        />
+        <button className="SearchButton" onClick={search}>
+          SEARCH
+        </button>
+      </div>
     </div>
   );
 }
