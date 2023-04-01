781d59442337ad102876ab873ea02c43fee18e52
Deron <deronmcguire23@yahoo.com>
2023-03-31 23:59:16 -0500

    Added option to render PlaylistList component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,27 +1,49 @@
 import React from "react";
 import "./Playlist.css";
 
+import PlaylistList from "../PlaylistList/PlaylistList";
 import TrackList from "../TrackList/TrackList";
 
 export default function Playlist({
+  playlists,
+  playlistSelection,
   playlistName,
   playlistTracks,
   onRemove,
   onNameChange,
   onSave,
 }) {
+  let playlistType =
+    playlistSelection === "New Playlist" ? "New Playlist" : "User's Playlist";
+
   function handleNameChange(e) {
     onNameChange(e.target.value);
+  }
+
+  function renderPlaylistSelection() {
+    if (playlistType === "New Playlist") {
+      return (
+        <TrackList
+          tracks={playlistTracks}
+          onRemove={onRemove}
+          isRemoval={true}
+        />
+      );
+    } else {
+      return (
+        <PlaylistList playlists={playlists} playlistDetails={playlistDetails} />
+      );
+    }
   }
 
   return (
     <div className="Playlist">
       <input
-        placeholder="New Playlist"
+        placeholder={playlistType}
         onChange={handleNameChange}
         value={playlistName}
       />
-      <TrackList tracks={playlistTracks} onRemove={onRemove} isRemoval={true} />
+      {renderPlaylistSelection()}
       <button className="Playlist-save" onClick={onSave}>
         SAVE TO SPOTIFY
       </button>
