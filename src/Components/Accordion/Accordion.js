fffb9a234842160df5d1a81bf7404c3ef43f31f4
Deron <deronmcguire23@yahoo.com>
2023-05-09 16:41:30 -0500

    Got accordion component working with showing playlist track list

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -1,13 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./Accordion.css";
+
+import Spotify from "../../util/Spotify";
 
 export default function Accordion(props) {
-  const [isActive, setIsActive] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [playlistDetails, setPlaylistDetails] = useState();
+
+  useEffect(() => {
+    getPlaylistTracks(props.playlistId);
+  }, [props.playlistId]);
+
+  function getPlaylistTracks(playlistId) {
+    Spotify.getPlaylistDetails(playlistId).then((tracks) => {
+      setPlaylistDetails(tracks);
+    });
+  }
+
+  function renderAccordion() {
+    if (playlistDetails) {
+      return playlistDetails.map((playlistTrack) => {
+        return (
+          <div className="list-item">
+            <p>{playlistTrack.songName}</p>
+          </div>
+        );
+      });
+    }
+  }
+
   return (
-    <div className="accordion">
-      <div className="accordion-title">
-        <div className="content">
-          <p>Testing</p>
+    <div className="accordion-container">
+      <div className="accordion">
+        <div
+          className="accordion-title"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {"Playlist #1"}
         </div>
+      </div>
+      <div className={`content ${isVisible ? "show-content" : ""}`}>
+        {renderAccordion()}
+        {/* <div className="list-item">
+          <p>Item 1</p>
+        </div>
+        <div className="list-item">
+          <p>Item 2</p>
+        </div>
+        <div className="list-item">
+          <p>Item 3</p>
+        </div>
+        <div className="list-item">
+          <p>Item 1</p>
+        </div>
+        <div className="list-item">
+          <p>Item 2</p>
+        </div>
+        <div className="list-item">
+          <p>Item 3</p>
+        </div> */}
       </div>
     </div>
   );
